8721551cac971194aa7f7f86c560d3aa6911bd49
retailescapeartist <retailescapeartist@gmail.com>
2023-12-02 04:44:49 -0500

    more responsive mobile work done

--- src/Pages/GameReviewsPage.jsx
+++ src/Pages/GameReviewsPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import './GameReviewsPage.css'
 
 const GameReviewsPage = () => {
   return (
-    <div>GameReviewsPage</div>
+    <div>
+      <h1>Game Reviews</h1>
+    </div>
   )
 }
 
--- src/Pages/RedemptionsListPage.jsx
+++ src/Pages/RedemptionsListPage.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const TwitchRedemptionsListPage = () => {
   return (
-    <div>TwitchRedemptionsListPage</div>
+    <div>
+      <h1>Twitch Redemptions</h1>
+      <p>!sfx for a list of alerts, scarier ones are sub only
+
+!quote add, !quote, !fact (for a random interesting fact!), !ethics, !uplay, !switch, !steam
+
+Subscriber Commands:
+!no, !burn, !scary, !laugh1, !goofy, !scream1, !scream2, !ermey, !chaos, !saber, !iseeyou, !chain, !space, !growly, !exterminate, !snarl, !dance, !mercy, !cat, !zombie, !newt, !tequila, !sacred, !scratch
+
+Subs get their own sounds!
+
+Follower sound alert cooldowns are 60 min, Sub sounds are 30 min. Except Rex, and she knows why! If you spam the alerts, they don't scare me!
+
+All bits get an alert, but bits 5-100 are one scare alert, one for 101-500, and 500+</p>
+    </div>
   )
 }
 
