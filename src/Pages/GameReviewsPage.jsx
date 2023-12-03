9db0517d9a61b2c49550695a9ac1f47d38398b7f
retailescapeartist <retailescapeartist@gmail.com>
2023-12-03 15:11:31 -0500

    more mobile design work done

--- src/Pages/GameReviewsPage.jsx
+++ src/Pages/GameReviewsPage.jsx
@@ -5,6 +5,44 @@ const GameReviewsPage = () => {
   return (
     <div>
       <h1>Game Reviews</h1>
+      <div className='reviews'>
+        <div>
+          <h3>Review #1</h3>
+          <div>
+            <h4>Title</h4>
+            <p>Description</p>
+            <p>Rating</p>
+            <p>Link to Game</p>
+          </div>
+        </div>
+        <div>
+          <h3>Review #1</h3>
+          <div>
+            <h4>Title</h4>
+            <p>Description</p>
+            <p>Rating</p>
+            <p>Link to Game</p>
+          </div>
+        </div>
+        <div>
+          <h3>Review #1</h3>
+          <div>
+            <h4>Title</h4>
+            <p>Description</p>
+            <p>Rating</p>
+            <p>Link to Game</p>
+          </div>
+        </div>
+        <div>
+          <h3>Review #1</h3>
+          <div>
+            <h4>Title</h4>
+            <p>Description</p>
+            <p>Rating</p>
+            <p>Link to Game</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/Pages/SchedulePage.jsx
+++ src/Pages/SchedulePage.jsx
@@ -1,16 +1,44 @@
-import "../App.css"
+import "./SchedulePage.css"
 import React from 'react';
 
 const SchedulePage = () => {
   return (
     <div className="app-container">
       <h1>Schedule</h1>
-      <p>Domino’s Attempt at a Schedule! (all times eastern)(UTC – 4hrs)</p>
-
-      <p>Current long stream days: Tuesday, Wednesday, Saturday</p>
       <p>On these days, I will try to start at 2 EDT (UTC - 4).</p>
 
       <p>Thank you for your patience. I want to stream more, but fear of pain keeps holding me back. And the hiccups. The FRACKIN HICCUPS!!!! lol. (diaphragm is also a muscle which can spasm)</p>
+      <div className="schedule">
+        <div>
+        <h1>Sunday</h1>
+        <p>Stream Time:</p>
+      </div>
+      <div>
+        <h1>Monday</h1>
+        <p>Stream Time:</p>
+      </div>
+      <div>
+        <h1>Tuesday</h1>
+        <p>Stream Time:</p>
+      </div>
+      <div>
+        <h1>Wednesday</h1>
+        <p>Stream Time:</p>
+      </div>
+      <div>
+        <h1>Thursday</h1>
+        <p>Stream Time:</p>
+      </div>
+      <div>
+        <h1>Friday</h1>
+        <p>Stream Time:</p>
+      </div>
+      <div className="saturday">
+        <h1>Saturday</h1>
+        <p>Stream Time:</p>
+      </div>
+      </div>
+      
     </div>
   );
 };
