89bba31471da24754c8e949d0c53dbb4276b3270
retailescapeartist <retailescapeartist@gmail.com>
2024-02-24 02:09:36 -0500

    updated clip of the week for YouTube

--- src/ClipsPlaylistsPage.jsx
+++ src/ClipsPlaylistsPage.jsx
@@ -19,7 +19,7 @@ const ClipsPlaylistsPage = () => {
           <div className="clip-player-youtube">
             {/* YouTube video */}
             <ReactPlayer
-              url="<https://www.youtube.com/shorts/JCdPkhP5wz8>"
+              url="<https://www.youtube.com/shorts/XuFUgz-OmdA>"
               width="100%"
               volume="0"
               controls
