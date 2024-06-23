3ec8868cd727148f4dc171124ae4e2652e4a4dec
retailescapeartist <retailescapeartist@gmail.com>
2024-06-23 01:19:07 -0400

    Images for art gallery updated

--- src/ArtShowcasePage.jsx
+++ src/ArtShowcasePage.jsx
@@ -30,12 +30,12 @@ const ArtShowcasePage = () => {
           >
             <img
               className="gallery-grid-col-2 gallery-grid-row-2"
-              src="https://cdn.discordapp.com/attachments/368111643193901057/1218655348941127720/AncientConstructs.jpg?ex=6611aebc&is=65ff39bc&hm=80a4cbda3448f38ae9e9d64bb8974debac1b70c9a61d801985e0bf20d1f17d49&"
+              src="https://cdn.discordapp.com/attachments/368111643193901057/1249888967239925802/20240610_174924.jpg?ex=6678c34f&is=667771cf&hm=83df440d8e0da56a15d5819ff0324f4ed2534a26ce5d836968422f11737c27c2&"
               alt=""
             ></img>
             <div className="overlay">
-              <h1 className="art-title">Model Painting</h1>
-              <p>done by Undead</p>
+              <h1 className="art-title">Moons & Cards</h1>
+              <p>done by Mrs_Crunch</p>
             </div>
           </div>
 
@@ -45,42 +45,12 @@ const ArtShowcasePage = () => {
           >
             <img
               className="gallery-grid-col-2 gallery-grid-row-2"
-              src="https://cdn.discordapp.com/attachments/368111643193901057/1218620372505591848/IMG_20240316_135940969.jpg?ex=66118e29&is=65ff1929&hm=3efc462c85de032c1be8c8f41a3c1ec73c0bef3d3c63fb916d5e9f4f72db7aed&"
+              src="https://media.discordapp.net/attachments/368111643193901057/1240769649990307910/Tattoo_Idea_Outline_1.png?ex=66788bc8&is=66773a48&hm=6ccebe91e160574b766bf94925465971c12f438ad50962081320a1f4ce10c3b1&=&format=webp&quality=lossless&width=670&height=468"
               alt=""
             ></img>
             <div className="overlay">
-              <h1 className="art-title">Random Squid Sketch</h1>
-              <p>done by KingSquiddie</p>
-            </div>
-          </div>
-
-          <div
-            className="gallery-item gallery-grid-row gallery-grid-col"
-            tabindex="0"
-          >
-            <img
-              className="gallery-grid-col-2 gallery-grid-row-2"
-              src="https://cdn.discordapp.com/attachments/368111643193901057/1205341425856675870/cool_dragon.jpg?ex=660f63ae&is=65fceeae&hm=b1f3c90086876496e7cea3cabf1772e5ca2c7478b5f4d85cfe98768b94092408&"
-              alt=""
-            ></img>
-            <div className="overlay">
-              <h1 className="art-title">Dragon with the roses</h1>
-              <p>By MrsCrunch</p>
-            </div>
-          </div>
-
-          <div
-            className="gallery-item gallery-grid-row gallery-grid-col"
-            tabindex="0"
-          >
-            <img
-              className="gallery-grid-col-2 gallery-grid-row-2"
-              src="https://cdn.discordapp.com/attachments/368111643193901057/1177081512223588413/20231122_184553.jpg?ex=660e1815&is=65fba315&hm=8262f2c67fceee54f4062db0a74cd6061a1cbd6ea7ec2c2da63d8ffdb0e38838&"
-              alt=""
-            ></img>
-            <div className="overlay">
-              <h1 className="art-title">Sunset Diamond Painting</h1>
-              <p>By MrsCrunch</p>
+              <h1 className="art-title">Roses and Moonlight</h1>
+              <p>done by JayFox</p>
             </div>
           </div>
 
